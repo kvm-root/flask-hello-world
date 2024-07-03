525dbeb3caaf9121ad632fbfa5b265fdfaa35dc2
kvm-root <161141097+kvm-root@users.noreply.github.com>
2024-07-03 17:04:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello, World!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'Kratos' and password == 'KingV000n':
+            return redirect('http://www.welpeckroot.cn')
+        else:
+            return 'Invalid credentials. Please try again.'
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
